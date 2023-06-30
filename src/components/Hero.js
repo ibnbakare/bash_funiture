bac561cf206a1036c533fa4e7ce47e55c8f9ad01
ridhwan <ridmartrite@gmail.com>
2023-06-30 17:45:38 +0100

    done with the stat section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 // import data
-
 import {hero} from "../data"
+
+// Import components
+import Stats from "./Stats"
+
 const Hero = () => {
   // destructure hero
   const {title,subtitle,buttonText} = hero
@@ -12,8 +15,18 @@ const Hero = () => {
       <div className="container mx-auto text-center  ">
         {/* title */}
        <h1 className="text-2xl mx-auto font-semibold lg:text-[64px] mb-[30px] lg:leading-tight"> {title}</h1>
-    
-        
+
+       {/* subtitle */}
+       <h2 className="mb-[30px] lg:mb-[65px] max-w-[627px] mx-auto lg:text-xl">
+        {subtitle}
+       </h2>
+       {/* button */}
+       <button className="bg-[rgba(255,255,255,0.4)] px-[35px] py-[9px] rounded-md hover:bg-[rgba(255,255,255,0.5)] mb-[160px] text-xl backdrop-blur-sm lg:px-[80px] lg:py-[16px] lg:mb-[194px]">
+        {buttonText}
+       </button>
+        <div className="text-black">
+          <Stats />
+        </div>
       </div>
     </section>
   )
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+// Import data
+
+ import {stats} from "../data"
+
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <div className="bg-accent p-12 rounded-[20px] text-black">
+      <div className="flex flex-wrap gap-y-8 ">
+        {stats.map((items,index) =>{
+          return(
+            <div key={index} className="flex flex-col justify-center w-3/6 odd:border-r lg:flex-1 lg:even:border-r lg:even:last:border-none min-h-[70px]">
+              <div>{items.value}</div>
+              <div>{items.text}</div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
