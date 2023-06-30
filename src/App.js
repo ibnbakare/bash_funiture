8d1cbb4eb2a0354058bf065802db0d898cc049c6
ridhwan <ridmartrite@gmail.com>
2023-06-30 18:53:38 +0100

    feature section

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 // import components
 
 import Hero from "./components/Hero"
+import Features from "./components/Features"
 
 const App = () => {
   return <div className="w-full max-w-[100%]  bg-white">
     < Hero />
+    <Features />
   </div>;
 };
 
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -11,8 +11,8 @@ const Stats = () => {
         {stats.map((items,index) =>{
           return(
             <div key={index} className="flex flex-col justify-center w-3/6 odd:border-r lg:flex-1 lg:even:border-r lg:even:last:border-none min-h-[70px]">
-              <div>{items.value}</div>
-              <div>{items.text}</div>
+              <div className="text-2xl font-semibold lg:text-4xl" >{items.value}</div>
+              <div className="text-base font-light " >{items.text}</div>
             </div>
           )
         })}
